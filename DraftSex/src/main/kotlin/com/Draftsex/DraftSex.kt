40c28219204f6ad8a0591b5fac44b0cecc901f75
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-04-15 09:58:23 +0530

    Again

--- DraftSex/src/main/kotlin/com/Draftsex/DraftSex.kt
+++ DraftSex/src/main/kotlin/com/Draftsex/DraftSex.kt
@@ -65,9 +65,9 @@ class DraftSex : MainAPI() {
     override suspend fun load(url: String): LoadResponse {
         val document = app.get(url).document
 
-        val title       = document.selectFirst("meta[property=og:title]")?.attr("content")
+        val title       = document.selectFirst("meta[property=og:title]")?.attr("content")?.trim().toString()
         val poster      = fixUrlNull(document.selectFirst("meta[property=og:image]")?.attr("content"))
-        val description = document.selectFirst("meta[property=og:description]")?.attr("content")
+        val description = document.selectFirst("meta[property=og:description]")?.attr("content")?.trim()
 
 
         return newMovieLoadResponse(title, url, TvType.NSFW, url) {
