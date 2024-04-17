51636563a1038dda56959739bd59e5dbe48b5277
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-04-17 13:58:29 +0530

    Update FullPorner.kt

--- FullPorner/src/main/kotlin/com/KillerDogeEmpire/FullPorner.kt
+++ FullPorner/src/main/kotlin/com/KillerDogeEmpire/FullPorner.kt
@@ -78,8 +78,8 @@ class FullPorner : MainAPI() {
 
     val videoID = Regex("""var id = \"(.+?)\"""").find(iframeDocument.html())?.groupValues?.get(1)
     val pornTrexDocument = app.get("https://www.porntrex.com/embed/${videoID}").document
-    val imgTag = pornTrexDocument.selectFirst("#kt_player > div.fp-player > div.fp-poster > img")
-    val poster = imgTag?.attr("src")
+    val poster = fixUrlNull(Regex("""src=["']([^"']+\.(?:jpg|png))["']""").find(pornTrexDocument.html())?.groupValues?.get(1))
+
     val posterUrl = fixUrlNull("https:$poster")
 
     val tags = document.select("div.video-block div.single-video-left div.single-video-title p.tag-link span a").map { it.text() }
