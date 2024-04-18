de0ca337689c30f398101f10edba1405f4f257ca
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-04-18 11:06:33 +0530

    Update LuxMovies.kt

--- LuxMovies/src/main/kotlin/com/LuxMovies/LuxMovies.kt
+++ LuxMovies/src/main/kotlin/com/LuxMovies/LuxMovies.kt
@@ -4,7 +4,7 @@ import org.jsoup.nodes.Element
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
 
-class DraftSex : MainAPI() {
+class LuxMovies : MainAPI() {
     override var mainUrl              = "https://luxmovies.art/"
     override var name                 = "LuxMovies"
     override val hasMainPage          = true
