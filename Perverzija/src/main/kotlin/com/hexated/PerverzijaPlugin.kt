eeedb343db46bfe2ee89c1db058fc9533e310ec5
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-04-19 11:49:48 +0530

    fix

--- Perverzija/src/main/kotlin/com/hexated/PerverzijaPlugin.kt
+++ Perverzija/src/main/kotlin/com/hexated/PerverzijaPlugin.kt
@@ -9,5 +9,6 @@ class PerverzijaPlugin : Plugin() {
     override fun load(context: Context) {
         // All providers should be added in this manner. Please don't edit the providers list directly.
         registerMainAPI(Perverzija())
+        registerExtractorAPI(Xtremestream())
     }
 }
