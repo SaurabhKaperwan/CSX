6715d1f20b16226978a439965d7cbb699039b9d8
Saurabh Kaperwan <91174352+SaurabhKaperwan@users.noreply.github.com>
2024-04-19 16:45:34 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com//SaurabhKaperwan/CS")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com//SaurabhKaperwan/CSX")
 
         authors = listOf("megix")
     }
