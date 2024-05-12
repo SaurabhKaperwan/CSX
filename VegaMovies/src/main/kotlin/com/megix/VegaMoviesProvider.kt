a47238ec69879a783f26ef7e6fc3058c9ece42e4
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-05-12 21:44:18 +0530

    Update VegaMoviesProvider.kt

--- VegaMovies/src/main/kotlin/com/megix/VegaMoviesProvider.kt
+++ VegaMovies/src/main/kotlin/com/megix/VegaMoviesProvider.kt
@@ -104,9 +104,9 @@ open class VegaMoviesProvider : MainAPI() { // all providers must be an instance
 
             for(HPTag in HPTags) {
                 val realSeasonRegex = Regex("""(?:Season |S)(\d+)""")
-                val realSeason = realSeasonRegex.find(HPTag)?.groupValues?.get(1)
+                val realSeason = realSeasonRegex.find(HPTag)?.groupValues?.get(1) ?: "Unknown"
                 val qualityRegex = Regex("""1080p|720p|480p|2160p|4K""")
-                val quality = qualityRegex.find(HPTag)?.value
+                val quality = qualityRegex.find(HPTag)?.value ?: "Unknown"
 
                 var Eurl = ""
 
