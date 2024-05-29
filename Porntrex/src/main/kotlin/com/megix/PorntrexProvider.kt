00ee1e901ca43997bb8941ca419177d32505da6b
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-05-29 18:19:38 +0530

    try

--- Porntrex/src/main/kotlin/com/megix/PorntrexProvider.kt
+++ Porntrex/src/main/kotlin/com/megix/PorntrexProvider.kt
@@ -1,7 +1,6 @@
 package com.megix
 
 import android.content.Context
-import com.KillerDogeEmpire.Porntrex
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
