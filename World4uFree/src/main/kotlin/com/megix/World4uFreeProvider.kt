8a0a16129dcc18dff79bf780ee57625d54ca956b
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-08-24 16:12:15 +0530

    World4uFree : Some Fixes

--- World4uFree/src/main/kotlin/com/megix/World4uFreeProvider.kt
+++ World4uFree/src/main/kotlin/com/megix/World4uFreeProvider.kt
@@ -67,10 +67,10 @@ class World4uFreeProvider : MainAPI() { // all providers must be an instance of 
         val document = app.get(url).document
         val title = document.selectFirst("meta[property=og:title]").attr("content").replace("Download ", "")
         val div = document.selectFirst("div.entry-content")
-        val plot = div.selectFirst("p:matches((?i)(plot|synopsis|story))").text()
-
+        val plot = div ?. selectFirst("p:matches((?i)(plot|synopsis|story))") ?. text() ?: ""
         var posterUrl = document.selectFirst("meta[property=og:image]").attr("content").toString()
-        if(posterUrl.isEmpty()) {
+
+        if(posterUrl.isEmpty() || posterUrl.contains("$mainUrl/favicon-32x32.png")) {
             posterUrl = document.selectFirst("div.separator > a > img").attr("data-src").toString()
         }
         val tvType = if (document.select("div.entry-content").text().contains("movie name", ignoreCase = true)) {
