03c1efd6d9b7f610e581c032821a3034b27fa825
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2024-11-21 12:16:06 +0530

    Remove dead sources

--- Deadstream/src/main/kotlin/com/Deadstream/Extractor.kt
+++
@@ -1,9 +0,0 @@
-package com.Deadstream
-
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.extractors.Filesim
-
-class MyFileMoon : Filesim() {
-    override var mainUrl = "https://filemoon.nl"
-}
