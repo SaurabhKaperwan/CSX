caeb532fee92340836e4c60809efd4b6f1dfb566
Nexus <ranamandal018@proton.me>
2025-01-31 09:38:21 +0530

    Update Extractors.kt

--- Mlsbd/src/main/kotlin/com/megix/Extractors.kt
+++ Mlsbd/src/main/kotlin/com/megix/Extractors.kt
@@ -20,18 +20,109 @@ open class GDFlix : ExtractorApi() {
     override val mainUrl: String = "https://new4.gdflix.cfd"
     override val requiresReferer = false
 
-    private suspend fun extractbollytag(url: String): String = withContext(Dispatchers.IO) {
-        app.get(url).text.let { tagdoc ->
-            """\b\d{3,4}p\b""".toRegex().find(tagdoc)?.value?.trim() ?: ""
-        }
+    private suspend fun extractbollytag(url:String): String {
+        val tagdoc= app.get(url).text
+        val tags ="""\b\d{3,4}p\b""".toRegex().find(tagdoc) ?. value ?. trim() ?:""
+        return tags
     }
 
-    private suspend fun extractbollytag2(url: String): String = withContext(Dispatchers.IO) {
-        app.get(url).text.let { tagdoc ->
-            """\b\d{3,4}p\b\s(.*?)\[""".toRegex().find(tagdoc)?.groupValues?.get(1)?.trim() ?: ""
-        }
+    private suspend fun extractbollytag2(url:String): String {
+        val tagdoc= app.get(url).text
+        val tags ="""\b\d{3,4}p\b\s(.*?)\[""".toRegex().find(tagdoc) ?. groupValues ?. get(1) ?. trim() ?:""
+        return tags
     }
 
-    private suspend fun extractFastCloud(element: Element, tagquality: String, tags: String, callback: (ExtractorLink) -> Unit) {
-        val link = element.attr("href")
-        val trueurl = app.get("$mainUrl$link", timeout = 30L).document
+    override suspend fun getUrl(
+        url: String,
+        referer: String?,
+        subtitleCallback: (SubtitleFile) -> Unit,
+        callback: (ExtractorLink) -> Unit
+    ) {
+        var originalUrl = url
+        val tags = extractbollytag(originalUrl)
+        val tagquality = extractbollytag2(originalUrl)
+
+        if (originalUrl.startsWith("$mainUrl/goto/token/")) {
+            val partialurl = app.get(originalUrl).text.substringAfter("replace(\"").substringBefore("\")")
+            originalUrl = mainUrl + partialurl
+        }
+        app.get(originalUrl).document.select("div.text-center a").map {
+            if (it.select("a").text().contains("FAST CLOUD DL"))
+            {
+                val link=it.attr("href")
+                val trueurl=app.get("$mainUrl$link", timeout = 30L).document.selectFirst("a.btn-success")?.attr("href") ?:""
+                callback.invoke(
+                    ExtractorLink(
+                        "GDFlix[Fast Cloud]",
+                        "GDFLix[Fast Cloud] $tagquality",
+                        trueurl,
+                        "",
+                        getQualityFromName(tags)
+                    )
+                )
+            }
+            else if (it.select("a").text().contains("DRIVEBOT LINK"))
+            {
+                val driveLink = it.attr("href")
+                val id = driveLink.substringAfter("id=").substringBefore("&")
+                val doId = driveLink.substringAfter("do=").substringBefore("==")
+                val indexbotlink = "https://indexbot.lol/download?id=${id}&do=${doId}"
+                val indexbotresponse = app.get(indexbotlink, timeout = 30L)
+                if(indexbotresponse.isSuccessful) {
+                    val cookiesSSID = indexbotresponse.cookies["PHPSESSID"]
+                    val indexbotDoc = indexbotresponse.document
+                    val token = Regex("""formData\.append\('token', '([a-f0-9]+)'\)""").find(indexbotDoc.toString()) ?. groupValues ?. get(1) ?: "token"
+                    val postId = Regex("""fetch\('/download\?id=([a-zA-Z0-9/+]+)'""").find(indexbotDoc.toString()) ?. groupValues ?. get(1) ?: "postId"
+
+                    val requestBody = FormBody.Builder()
+                        .add("token", token)
+                        .build()
+
+                    val headers = mapOf(
+                        "Referer" to indexbotlink
+                    )
+
+                    val cookies = mapOf(
+                        "PHPSESSID" to "$cookiesSSID",
+                    )
+
+                    val response = app.post(
+                        "https://indexbot.lol/download?id=${postId}",
+                        requestBody = requestBody,
+                        headers = headers,
+                        cookies = cookies,
+                        timeout = 30L
+                    ).toString()
+
+                    var downloadlink = Regex("url\":\"(.*?)\"").find(response) ?. groupValues ?. get(1) ?: ""
+
+                    downloadlink = downloadlink.replace("\\", "")
+
+                    callback.invoke(
+                        ExtractorLink(
+                            "GDFlix[IndexBot](VLC)",
+                            "GDFlix[IndexBot](VLC) $tagquality",
+                            downloadlink,
+                            "https://indexbot.lol/",
+                            getQualityFromName(tags)
+                        )
+                    )
+                }
+            }
+            else if (it.select("a").text().contains("Instant DL"))
+            {
+                val instantLink = it.attr("href")
+                val link = app.get(instantLink, timeout = 30L, allowRedirects = false).headers["Location"]?.split("url=") ?. getOrNull(1) ?: ""
+                callback.invoke(
+                    ExtractorLink(
+                        "GDFlix[Instant Download]",
+                        "GDFlix[Instant Download] $tagquality",
+                        link,
+                        "",
+                        getQualityFromName(tags)
+                    )
+                )
+            }
+        }
+    }
+}
