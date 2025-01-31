c0b4c74cabf57670750a6cb698062f7192f874e5
Nexus <ranamandal018@proton.me>
2025-01-31 09:37:15 +0530

    Update Extractors.kt

--- Mlsbd/src/main/kotlin/com/megix/Extractors.kt
+++ Mlsbd/src/main/kotlin/com/megix/Extractors.kt
@@ -6,9 +6,6 @@ import com.lagradost.cloudstream3.utils.ExtractorApi
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.getQualityFromName
 import okhttp3.FormBody
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import org.jsoup.select.Elements
 
 class GDFlix1 : GDFlix() {
     override val mainUrl: String = "https://new3.gdflix.cfd"
