5109a8ae0853aeeb5ed1442b06f03dd7a1a96830
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2025-02-01 19:11:14 +0530

    Revert "fix"
    This reverts commit f19b5de968298f83623f67f19dbd5c72824e8c9d.

--- Mlsbd/src/main/kotlin/com/redowan/MlsbdProvider.kt
+++ Mlsbd/src/main/kotlin/com/redowan/MlsbdProvider.kt
@@ -1,12 +1,14 @@
 package com.megix
 
 import com.lagradost.cloudstream3.Episode
-import com.lagradost.cloudstream3.HomePageList
+import com.lagradost.cloudstream3
+.HomePageList
 import com.lagradost.cloudstream3.HomePageResponse
 import com.lagradost.cloudstream3.LoadResponse
 import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.MainPageRequest
-import com.lagradost.cloudstream3.SearchResponse
+import com.
+lagradost.cloudstream3.SearchResponse
 import com.lagradost.cloudstream3.SubtitleFile
 import com.lagradost.cloudstream3.TvType
 import com.lagradost.cloudstream3.app
@@ -14,10 +16,13 @@ import com.lagradost.cloudstream3.mainPageOf
 import com.lagradost.cloudstream3.newHomePageResponse
 import com.lagradost.cloudstream3.newMovieLoadResponse
 import com.lagradost.cloudstream3.newMovieSearchResponse
-import com.lagradost.cloudstream3.newTvSeriesLoadResponse
+import com.lagradost.cloudstream3.newTvSeries
+LoadResponse
 import com.lagradost.cloudstream3.utils.ExtractorLink
 import com.lagradost.cloudstream3.utils.loadExtractor
 import org.jsoup.nodes.Element
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 //suspend fun main() {
 //    val providerTester = com.lagradost.cloudstreamtest.ProviderTester(MlsbdProvider())
