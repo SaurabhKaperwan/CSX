4d2a5c2e9d91a1f9f9e55d50b00b216a7db4880b
Nexus <ranamandal018@proton.me>
2025-02-15 13:58:50 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -38,7 +38,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/SaurabhKaperwan/CSX")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/nexus-gitss/CSX")
 
         authors = listOf("megix")
     }
