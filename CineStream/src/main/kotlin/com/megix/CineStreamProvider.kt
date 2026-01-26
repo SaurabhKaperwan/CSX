c61324842ad61ef4309778fb7f0ba699414f2f0f
Saurabh Kaperwan <saurabhkaprawan4@gmail.com>
2026-01-26 13:06:43 +0530

    try

--- CineStream/src/main/kotlin/com/megix/CineStreamProvider.kt
+++ CineStream/src/main/kotlin/com/megix/CineStreamProvider.kt
@@ -5,6 +5,7 @@ import com.lagradost.cloudstream3.utils.*
 import com.lagradost.cloudstream3.LoadResponse.Companion.addImdbId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addAniListId
 import com.lagradost.cloudstream3.LoadResponse.Companion.addMalId
+import com.lagradost.cloudstream3.LoadResponse.Companion.addKitsuId
 import com.lagradost.cloudstream3.utils.AppUtils.toJson
 import com.lagradost.cloudstream3.utils.AppUtils.parseJson
 import com.lagradost.cloudstream3.utils.AppUtils.tryParseJson
@@ -350,6 +351,7 @@ open class CineStreamProvider : MainAPI() {
                 addAniListId(anilistId)
                 addMalId(malId)
                 addImdbId(id)
+                try { addKitsuId(kitsuId) } catch(_:Throwable){}
             }
         }
         else {
@@ -401,6 +403,7 @@ open class CineStreamProvider : MainAPI() {
                 addAniListId(anilistId)
                 addMalId(malId)
                 addImdbId(id)
+                try { addKitsuId(kitsuId) } catch(_:Throwable){}
             }
         }
     }
